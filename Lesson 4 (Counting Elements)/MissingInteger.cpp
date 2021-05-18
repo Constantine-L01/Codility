f11d6292f0376f7365c2ac9b7e182a484ecc5c31
Constantine-L01 <77671482+Constantine-L01@users.noreply.github.com>
2021-05-19 04:49:24 +0800

    Create MissingInteger.cpp

---
+++ Lesson 4 (Counting Elements)/MissingInteger.cpp
@@ -0,0 +1,41 @@
+Task description
+
+This is a demo task.
+
+Write a function:
+
+    int solution(vector<int> &A);
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+        N is an integer within the range [1..100,000];
+        each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+==Solution==
+// you can use includes, for example:
+#include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    int min = 1;
+    sort(A.begin(), A.end());
+
+    for(int i = 0; i < A.size(); i++){
+        if(min == A[i]){
+            min++;
+        }
+    }
+
+    return min;    
+}
